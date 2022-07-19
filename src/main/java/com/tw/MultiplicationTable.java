17b40693445090297ac1c2490313f44d4969dacb
ELIO QIU (ITA-ISDC-ISD-OOCLL/ZHA) <QIUEL3@OOCL.COM>
2022-07-19 16:18:09 +0800

    feat: implement generate multiplication table

--- src/main/java/com/tw/MultiplicationTable.java
+++ src/main/java/com/tw/MultiplicationTable.java
@@ -21,14 +21,32 @@ public class MultiplicationTable {
     }
 
     public String generateTable(int start, int end) {
-        return null;
+        StringBuffer sb = new StringBuffer();
+        for (int i = start; i <= end; i++) {
+            String line = generateLine(start, i);
+            if (i == end) {
+                sb.append(line);
+            } else {
+                sb.append(line).append("\r\n");
+            }
+        }
+        return sb.toString();
     }
 
     public String generateLine(int start, int row) {
-        return null;
+        StringBuffer sb = new StringBuffer();
+        for (int i = start; i <= row; i++) {
+            String expression = generateSingleExpression(i, row);
+            if (i == row) {
+                sb.append(expression);
+            } else {
+                sb.append(expression).append("  ");
+            }
+        }
+        return sb.toString();
     }
 
     public String generateSingleExpression(int multiplicand, int multiplier) {
-        return null;
+        return multiplicand + "*" + multiplier + "=" + multiplicand * multiplier;
     }
 }
